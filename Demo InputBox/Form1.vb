0d4847b101d6f6d54933e5ba5a5001f4572e9043
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-07-22 21:18:37 +0700

    Add project files.

---
+++ Demo InputBox/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim strMasukanUser As String
+        Dim dblNilai As Double
+        strMasukanUser = InputBox("Masukkan sebuah nilai.", "Silahkan Merespon")
+        dblNilai = CDbl(strMasukanUser)
+    End Sub
+End Class
